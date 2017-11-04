f49416f9535398e21653c32b6f93256bd8ae7ed5
ConnorPr84 <connorpr.84@gmail.com>
2017-11-03 21:45:00 -0400

    Added text updating for the account balance

--- app/src/main/java/code_for_good/wholesomewave/AccountInfo.java
+++ app/src/main/java/code_for_good/wholesomewave/AccountInfo.java
@@ -3,12 +3,21 @@ package code_for_good.wholesomewave;
 import android.app.Activity;
 import android.content.Context;
 
+import android.widget.TextView;
+
 import android.support.v7.widget.Toolbar;
+
+import org.w3c.dom.Text;
 
 public class AccountInfo extends MenuData
 {
     // instance variable for the patient's balance
     private double balance;
+
+    // instantiate TextView objects
+    TextView accountNumber = (TextView)activity.findViewById(R.id.tvAccountNumber);
+    TextView accountHolder = (TextView)activity.findViewById(R.id.tvAccountHolder);
+    TextView balanceText = (TextView)activity.findViewById(R.id.tvBalance);
 
     public AccountInfo(Context context, Activity activity)
     {
@@ -19,13 +28,18 @@ public class AccountInfo extends MenuData
         Toolbar toolbar = new Toolbar(activity);
         toolbar.setTitle("My Account");
 
+        // set balance to zero
         balance = 0;
     }
 
     // method updates the patient's balance
     public void updateBalance(double amount)
     {
+        // add the passed amount (even negative) to the account balance
         balance += amount;
 
+        // update the balanceText TextView to display the updated balance
+        String newText = R.string.balance + balance + "";
+        balanceText.setText(newText);
     }
 }

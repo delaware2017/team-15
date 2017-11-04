5c9086162023eddf24127db00c3be6dbe135a21d
MetalMario137 <KevinMario117@gmail.com>
2017-11-03 22:43:42 -0400

    added some menu and app functionality

--- app/src/main/java/code_for_good/wholesomewave/LoginValidator.java
+++ app/src/main/java/code_for_good/wholesomewave/LoginValidator.java
@@ -1,9 +1,16 @@
 package code_for_good.wholesomewave;
 
 
-public class LoginValidator {
+public class LoginValidator implements Validator{
 
     public LoginValidator(){
+
+    }
+
+    public boolean isTrue() {
+
+        //temporarily returns true
+        return true;
 
     }
 }
--- app/src/main/java/code_for_good/wholesomewave/MainActivity.java
+++ app/src/main/java/code_for_good/wholesomewave/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
     Context context;
     Activity activity;
+    Validator valid;
     LoginMenu loginMenu;
     AccountInfo accountInfo;
 
@@ -28,15 +29,31 @@ public class MainActivity extends AppCompatActivity {
 
 
     protected void testButton(View button) {
-        switch (button.getId()){
+        /*switch (button.getId()){
 
-        }
+        }*/
     }
     protected void verifyLogin(View button) {
 
+        /*valid = new LoginValidator();
+        if (valid.isTrue()) {
+            setContentView()
+        }*/
+    }
+    protected void changeMenu(View button){
         updateContexts();
-        loginMenu = new LoginMenu(context, activity);
+        switch (button.getId()) {
+            case R.id.to_account_info_button:
+                setContentView(R.layout.account_info);
+                accountInfo = new AccountInfo(context, activity);
+                break;
+            case R.id.to_fruit_basket_button: /*setContentView(R.layout.fruit_basket);*/ break;
+            case R.id.to_redemption_menu: break;
 
-
+        }
+    }
+    protected void toMainMenu(View button) {
+        updateContexts();
+        setContentView(R.layout.activity_main);
     }
 }

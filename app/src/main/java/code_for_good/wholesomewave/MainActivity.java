5ce6c2ace77e062a1cc512ebefa65f8ef0c2b264
MetalMario137 <KevinMario117@gmail.com>
2017-11-03 20:41:23 -0400

    added context method

--- app/src/main/java/code_for_good/wholesomewave/MainActivity.java
+++ app/src/main/java/code_for_good/wholesomewave/MainActivity.java
@@ -1,13 +1,25 @@
 package code_for_good.wholesomewave;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+    Context context;
+    Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        updateContexts();
+    }
+
+    private void updateContexts(){
+        context = getApplicationContext();
+        activity = MainActivity.this;
     }
 }

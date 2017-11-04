504c72bebab9540cd82d27f61c0058a7b8437b2f
ConnorPr84 <cprice18@dtcc.edu>
2017-11-04 06:59:10 -0400

    Started framework for QR screen back button

--- app/src/main/java/code_for_good/wholesomewave/QR.java
+++ app/src/main/java/code_for_good/wholesomewave/QR.java
@@ -2,14 +2,22 @@ package code_for_good.wholesomewave;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
 import android.widget.Button;
 
 public class QR
 {
     Button backButton;
 
-    public QR(Context context, Activity activity)
+    public QR(final Context context, final Activity activity)
     {
+        backButton = (Button)activity.findViewById(R.id.btBack);
 
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Balance balance = new Balance(context, activity);
+
+            }
+        });
     }
 }

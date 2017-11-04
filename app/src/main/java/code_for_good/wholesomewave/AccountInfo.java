6d0d6e7fd367cb3c1c872564766b39f12ccf56b8
ConnorPr84 <connorpr.84@gmail.com>
2017-11-03 20:42:11 -0400

    Added AccountInfo class

---
+++ app/src/main/java/code_for_good/wholesomewave/AccountInfo.java
@@ -0,0 +1,14 @@
+package code_for_good.wholesomewave;
+
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by Connor on 11/3/2017.
+ */
+
+public class AccountInfo {
+    public AccountInfo()
+    {
+        //Toolbar toolbar = new Toolbar(getApplicationContext());
+    }
+}

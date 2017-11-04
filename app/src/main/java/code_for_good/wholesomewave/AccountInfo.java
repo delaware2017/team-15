7d17da50e58a00debc3bb83ef79a77f6db2cc553
ConnorPr84 <connorpr.84@gmail.com>
2017-11-03 21:16:13 -0400

    Started method for updating the patient's balance

--- app/src/main/java/code_for_good/wholesomewave/AccountInfo.java
+++ app/src/main/java/code_for_good/wholesomewave/AccountInfo.java
@@ -9,10 +9,25 @@ import android.support.v7.widget.Toolbar;
  * Created by Connor on 11/3/2017.
  */
 
-public class AccountInfo {
+public class AccountInfo
+{
+    // instance variable for patient's balance
+    double balance = 0;
+
+    //
+
+
     public AccountInfo(Context context, Activity activity)
     {
+        // create a new Toolbar and set the title
         Toolbar toolbar = new Toolbar(activity);
-        toolbar.setTitle("Account Information");
+        toolbar.setTitle("My Account");
+    }
+
+    // method updates the patient's balance
+    public void updateBalance(double amount)
+    {
+        balance += amount;
+
     }
 }
